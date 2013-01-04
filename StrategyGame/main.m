1a00fcc346a11f671a91bc97dbd21c91eedf7ec8
Ahmad al-Moraly <ahmedalmoraly@gmail.com>
2013-01-04 21:06:14 +0200

    Initial Commit

---
+++ StrategyGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StrategyGame
+//
+//  Created by Ahmad al-Moraly on 1/4/13.
+//  Copyright (c) 2013 Artgine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARAppDelegate class]));
+    }
+}
