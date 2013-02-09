64d982a43f250197f1b107d6ec57ea17b26e016a
shadyghalab <shadyghalab@gmail.com>
2013-02-09 14:50:42 +0200

    Technologies Cont. Adding PageControl

--- StrategyGame/GameMenu/ARGameMenuViewController.m
+++ StrategyGame/GameMenu/ARGameMenuViewController.m
@@ -12,11 +12,13 @@
 #import "ARLoginViewController.h"
 #import "MessagesViewController.h"
 #import "ARMemberProfileViewController.h"
+#import "ARTechnologiesViewController.h"
 
 @interface ARGameMenuViewController ()<UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scroll;
 @property (weak, nonatomic) IBOutlet UIButton *messagesButton;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
 
 @end
 
@@ -40,10 +42,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-        numberOfPages_ = 2;
-    [self createPagingControl];
-
+    
+    numberOfPages_ = 2;
+    self.pageControl.numberOfPages = numberOfPages_;
     self.scroll.pagingEnabled=YES;
+    self.scroll.delegate = self;
+    
     CGSize size = self.scroll.frame.size;
     self.scroll.contentSize = CGSizeMake(size.width * numberOfPages_ , 230);
     self.scroll.directionalLockEnabled = YES;
@@ -72,78 +76,29 @@
     [(id)self.parentViewController pushViewController:memberProfile animated:YES];
 }
 
+- (IBAction)technologies:(id)sender
+{
+    ARTechnologiesViewController *technology = [[ARTechnologiesViewController alloc] initWithNibName:@"ARTechnologiesViewController" bundle:nil];
+    [(id)self.parentViewController pushViewController:technology animated:YES];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)createPagingControl {
-    // programmatically add the page control
-    pageControl_ = [[DDPageControl alloc] init];
-    pageControl_.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
-    pageControl_.backgroundColor = [UIColor blueColor];
-	[pageControl_ setCenter:CGPointMake(self.view.bounds.size.height/2, self.view.bounds.size.height)] ;
-    [self.view addSubview: pageControl_];
-	[pageControl_ setNumberOfPages: numberOfPages_];
-	[pageControl_ addTarget: self action: @selector(changedValue:) forControlEvents: UIControlEventValueChanged] ;
-	[pageControl_ setDefersCurrentPageDisplay: YES];
-    [pageControl_ setHidesForSinglePage:YES];
-    [pageControl_ setCurrentPage: 0];
-	[pageControl_ setType: DDPageControlTypeOnFullOffFull];
-	[pageControl_ setOnColor:UIColorFromRGB(0x669900)];
-	[pageControl_ setOffColor:UIColorFromRGB(0x001500)];
-	[pageControl_ setIndicatorDiameter: 10.0f];
-	[pageControl_ setIndicatorSpace: 12];
-    pageControlBeingUsed_ = NO;
-}
-
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-	pageControlBeingUsed_ = NO;
-}
-
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-	pageControlBeingUsed_ = NO;
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)sender {
-    if (sender.tag==0) {
-        if (!pageControlBeingUsed_) {
-            // Switch the indicator when more than 50% of the previous/next page is visible
-            //calculate the same for the arrabic view
-            CGFloat pageWidth = self.scroll.frame.size.width;
-            int page = floor((self.scroll.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-            pageControl_.currentPage = page;
-            // if we are animating (triggered by clicking on the page control), we update the page control
-            [pageControl_ updateCurrentPageDisplay];
-            
-        }
-    }else{
-        if (!pageControlBeingUsed_) {
-            // Switch the indicator when more than 50% of the previous/next page is visible
-            //calculate the same for the arrabic view
-            CGFloat pageWidth = self.scroll.frame.size.width;
-            int page = floor((self.scroll.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-            pageControl_.currentPage = page;
-            // if we are animating (triggered by clicking on the page control), we update the page control
-            [pageControl_ updateCurrentPageDisplay];
-            
-        }
-    }
-}
-
-- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)aScrollView
+- (void)scrollViewDidScroll:(UIScrollView *)sender
 {
-    // if we are animating (triggered by clicking on the page control), we update the page control
-	[pageControl_ updateCurrentPageDisplay] ;
-    
+    CGFloat pageWidth = self.scroll.frame.size.width;
+    int page = floor((self.scroll.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    self.pageControl.currentPage = page;
 }
-
-
 
 - (void)viewDidUnload {
     [self setScroll:nil];
     [self setMessagesButton:nil];
+    [self setPageControl:nil];
     [super viewDidUnload];
 }
 @end
